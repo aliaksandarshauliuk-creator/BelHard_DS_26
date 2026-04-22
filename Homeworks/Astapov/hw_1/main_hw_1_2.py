4657ae546b8c50f295888624c275eb7782df1794
vladdx220 <192007588+vladdx220@users.noreply.github.com>
2026-04-22 18:59:31 +0300

    HW_1 сделал. Отправляю на проверку.

--- Homeworks/Astapov/hw_1/main_hw_1_2.py
+++ Homeworks/Astapov/hw_1/main_hw_1_2.py
@@ -1,18 +1,34 @@
-from random import randint
+import random
 
-def main_1():
-    x = float(input('Сторона А: '))
-    y = float(input('Сторона Б: '))
-    print('Площадь прямоугольника =', x * y)
-
-def main_2():
+def main():
     while True:
-        a = int(input('Загадайте число от 1 до 10:'))
-        if a == randint(1, 10):
-            print('Я угадал твое число', a)
-        else:
-            print('Делаю ещё попытку')
+        print("Загадайте число от 1 до 10 (я попробую угадать)")
+
+        number = random.randint(1, 10)
+        attempts = 0
+
+        while True:
+            attempts += 1
+            answer = input(f"Это число {number}? [введите: больше(>) / меньше(<) / да(=)]: ").lower()
+
+            if answer in ("да", '='):
+                print(f"Угадал за {attempts} попыток!\n")
+                break
+            elif answer in ("больше", '>'):
+                number += 1
+                if number > 10:
+                    number = 10
+            elif answer in ("меньше",'<'):
+                number -= 1
+                if number < 1:
+                    number = 1
+            else:
+                print("Пожалуйста, введите: больше(>), меньше(<) или да(=)")
+
+        again = input("Загадаете новое число? (да/нет): ").lower()
+        if again != "да":
+            print("Игра завершена.")
+            break
 
 if __name__ == '__main__':
-    # main_1()
-    main_2()
+    main()
