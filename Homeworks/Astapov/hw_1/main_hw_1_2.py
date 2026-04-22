913ca88c628a5137d84f729ca11b767ce75b8d11
vladdx220 <192007588+vladdx220@users.noreply.github.com>
2026-04-22 19:02:59 +0300

    HW_1 сделал. Отправляю на проверку.

--- Homeworks/Astapov/hw_1/main_hw_1_2.py
+++ Homeworks/Astapov/hw_1/main_hw_1_2.py
@@ -1,33 +1,35 @@
 import random
 
 def main():
+    print('Домашнее задание № 2')
+    print('Игра «Угадай число»')
     while True:
-        print("Загадайте число от 1 до 10 (я попробую угадать)")
+        print('Загадайте число от 1 до 10 (я попробую угадать)')
 
         number = random.randint(1, 10)
         attempts = 0
 
         while True:
             attempts += 1
-            answer = input(f"Это число {number}? [введите: больше(>) / меньше(<) / да(=)]: ").lower()
+            answer = input(f'Это число {number}? [введите: больше(>) / меньше(<) / да(=)]: ').lower()
 
-            if answer in ("да", '='):
-                print(f"Угадал за {attempts} попыток!\n")
+            if answer in ('да', '='):
+                print(f'Угадал за {attempts} попыток!\n')
                 break
-            elif answer in ("больше", '>'):
+            elif answer in ('больше', '>'):
                 number += 1
                 if number > 10:
                     number = 10
-            elif answer in ("меньше",'<'):
+            elif answer in ('меньше','<'):
                 number -= 1
                 if number < 1:
                     number = 1
             else:
-                print("Пожалуйста, введите: больше(>), меньше(<) или да(=)")
+                print('Пожалуйста, введите: больше(>), меньше(<) или да(=)')
 
-        again = input("Загадаете новое число? (да/нет): ").lower()
-        if again != "да":
-            print("Игра завершена.")
+        again = input('Загадаете новое число? (да/нет): ').lower()
+        if again != 'да':
+            print('Игра завершена.')
             break
 
 if __name__ == '__main__':
