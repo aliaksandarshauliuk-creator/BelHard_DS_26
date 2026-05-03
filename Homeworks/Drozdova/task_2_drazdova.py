22fe8753ca810d17c275730f617dc3537f144c74
Nata_Dr <68166472+Krocus-art@users.noreply.github.com>
2026-05-03 12:19:29 +0300

    Update task_2_drazdova.py
    Новая версия с оптимизацией кода:
    можно начать новую игру по желанию пользователя;
    значения на задаются в пределах от 1 до 10;
    исправлены отступы

--- Homeworks/Drozdova/task_2_drazdova.py
+++ Homeworks/Drozdova/task_2_drazdova.py
@@ -1,51 +1,49 @@
 import random
-
 from numpy._core.strings import upper
 
-numberGuess = [0,1,2,3,4,5,6,7,8,9,10]
-attemptNum = 1
 print(f'Игра Угадайка\n')
-userAnswer = upper(input('Загадай число от 0 до 10\n Загадала? - ответь - Д или Н \n '))
-try:
-    match userAnswer:
-        case 'Д':
-            print(f'your answer is {userAnswer}')
-            useAnswOne = upper(input(f'Ваше число меньше 6? - ответь - Д или Н \n'))
-            match useAnswOne:
-                case 'Д':
-                    print(f'your answer is {useAnswOne}')
-                    oddNum = [0, 1, 2, 3, 4, 5]
-                    attemptN = 1
-                    while attemptN < len(oddNum):
-                        taskNum = oddNum[attemptN]
-                        userAnswTwo = input(f'Вы загадали {taskNum} Д или Н \n')
-                        attemptN += 1
-                        if userAnswTwo == 'Д':
-                            print(f'Вы загадали  {taskNum}. Спасибо за игру')
-                            break
-                        else: continue
+while True:
+    userAnswer = input('Загадай число от 1 до 10\n Загадала? - ответь - Д/Н \n ').upper().strip()
+    try:
+        match userAnswer:
+            case 'Д':
+                print(f'your answer is {userAnswer}')
+                # Определить, в какой половине искать
+                is_less_than_6 = input('Ваше число меньше 6? (Д/Н): ').upper().strip()
 
+                if is_less_than_6 == 'Д':
+                    min_val, max_val = 1, 5
+                elif is_less_than_6 == 'Н':
+                    min_val, max_val = 6, 10
+                else:
+                    print(f'выш ответ {is_less_than_6} некорректен! \n ')
 
+                # Список возможных чисел для этой половины
+                possible_numbers = list(range(min_val, max_val + 1))
+                random.shuffle(possible_numbers)  # Перемешиваем для эффекта "угадывания"
 
-                case 'Н':
-                  print(f'your answer is {useAnswOne}')
-                  oddNum = [6, 7, 8, 9, 10]
-                  attemptI = 0
-                  while attemptI < len(oddNum):
-                    taskNumT: int = oddNum[attemptI]
-                    attemptI += 1
-                    userAnswTwo = input(f'Вы загадали {taskNumT} Д или Н\n')
-                    if userAnswTwo == 'Д':
-                        print(f'Вы загадали  {taskNumT}. Спасибо за игру')
-                        break
-                    elif userAnswTwo == 'Н':
-                        continue
+                found = False
+                for num in possible_numbers:
+                    ans = input(f'Вы загадали {num}? (Д/Н): ').upper().strip()
+                    if ans == 'Д':
+                       print(f'Ура! Число {num} угадано!')
+                       found = True
+                       break
 
-        case 'Н':
-            print(f'your answer is {userAnswer}. Очень жаль, до новой игры!')
-        case _:
-            print(f'your answer is {userAnswer} is not correct!')
-except ValueError as e:
- print("Сведения об исключении", e)
- exit()
+                if not found:
+                    print("Хм, вы забыли загаданное число?...\n")
+
+                        #  предложение новой игры
+                new_round = input('\nПродолжим игру? (Д/Н): ').strip().upper()
+                if new_round != 'Д':
+                    print('Спасибо за игру! До встречи.')
+                    break  # Выход из главного цикла while True
+            case 'Н':
+                print(f'Ваш ответ -- {userAnswer}. \n Очень жаль, до новой игры!')
+                exit()
+            case _:
+                print(f'Ваш ответ -- {userAnswer} -- не корректен!')
+    except ValueError as e:
+        print("Сведения об исключении", e)
+        exit()
 
